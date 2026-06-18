7c38018bddbfec07da09a03e877491619f9f9075
Yaasien78 <141853765+Yaasien78@users.noreply.github.com>
2026-06-18 16:27:41 +0700

    Refactor log message in POST request handler

--- app/api/payments/complete/route.ts
+++ app/api/payments/complete/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
   const { paymentId, txid } = await req.json()
-  console.log("Opsi 11 - Complete:", paymentId, txid)
-  
+  console.log("Complete:", paymentId, txid)
   return NextResponse.json({ status: 'completed' }, { status: 200 })
 }
