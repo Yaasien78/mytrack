5f61ee91bf52416a47b1a31629ec1acb5ad91abb
Yaasien78 <141853765+Yaasien78@users.noreply.github.com>
2026-06-23 16:59:17 +0700

    Load Pi SDK after interactive and add PiLoader

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,13 +1,29 @@
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import { useEffect, useState } from 'react'
+
+function PiLoader() {
+  const [piReady, setPiReady] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.Pi) {
+      window.Pi.init({ version: "2.0", sandbox: true });
+      setPiReady(true);
+    }
+  }, []);
+
+  return null;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script 
         src="https://sdk.minepi.com/pi-sdk.js" 
-        strategy="beforeInteractive" 
+        strategy="afterInteractive"
+        onLoad={() => console.log('Pi SDK loaded')}
       />
+      <PiLoader />
       <Component {...pageProps} />
     </>
   )
